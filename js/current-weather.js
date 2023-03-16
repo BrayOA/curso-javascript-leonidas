69d249de6e67d9b133da54b0f821e432fba2e268
BrayOA <borconarellano@gmail.com>
2023-03-15 18:57:25 -0800

    city

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,6 +1,23 @@
 import weather from '../data/current-weather.js'
+//weather.name
+
+function configCurrentWeather(weather){
+
+    //loader
+    //date
+    //city
+     const $currentWeatherCity = document.querySelector('#current-weather-city')
+     $currentWeatherCity.textContent = weather.name
+    //temp
+    //background
+
+}
+
 
 
 export default function currentWeather(){
-    console.log('hola mundo');
+
+    //GEO // API - weather // config
+    configCurrentWeather(weather)
+    console.log(weather);
 }
--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
-import currentWeather from "./current-weather.js";
+import weather from '../data/current-weather.js';
+
+function currentWeather(weather){
+    //loader
+    //date
+    //city
+    const $currentWeatherCity = document.querySelector('#current-weather-city')
+    $currentWeatherCity.textContent = ''
+    //temp
+    //background
+}
 
 
 
-
-
-
-currentWeather ();
+export default function currentWeather(){
+    // GEO //API -Weather // config
+    configCurrentWeather(weather)
+    console.log(weather);
+}
