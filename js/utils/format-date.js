7ffa5095609b222c3adeda8f7da930e8e1c6ee62
BrayOA <borconarellano@gmail.com>
2023-03-16 20:59:16 -0700

    renderisar la fecha y la hora y formatdate

---
+++ js/utils/format-date.js
@@ -0,0 +1,14 @@
+
+const defaultDateOptions = {
+    day: 'numeric',
+    weekday: 'long',
+    month: 'long'
+}
+
+ export  function formatDate (date, option = defaultDateOptions ) {
+   return new Intl.DateTimeFormat('es', option).format(date)
+    
+
+
+
+}
