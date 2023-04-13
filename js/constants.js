a4cea9f93caf5daee39d831a6327b911efaec828
BrayOA <borconarellano@gmail.com>
2023-04-13 14:31:23 -0700

    loading

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,6 @@
 
-
-
+export const API_KEY = '6beaa90da06b5aa4f4b6b7511d570983'
+export const BASE_API = 'https://api.openweathermap.org/data/2.5/'
 export const weatherConditionsCode = {
     2: 'rainy',
     3: 'drizzle',
--- js/current-weather.js
+++ js/current-weather.js
@@ -1,7 +1,8 @@
-import weather from '../data/current-weather.js'
+//import weather from '../data/current-weather.js'
 import { formatDate, formatTemp } from './utils/format-date.js'
 import {weatherConditionsCode} from './constants.js'
 import {getLatLon} from './geolocation.js'
+import {getCurrentWeather} from './services/weather.js'
 
 //weather.main.temp
 //weatherConditionsCode []
@@ -46,9 +47,16 @@ function setBackground ($el, conditioncode, solarStatus) {
     $el.style.backgroundImage = `url(./images/${solarStatus}-${weatherType}${size}.jpg)`
 }
 
-function configCurrentWeather(weather){
+function showCurrentWeather($app, $loading){
+    $app.hidden = false
+    $loading.hidden = true
+}
 
+function configCurrentWeather(weather){
+    const $app =document.querySelector('#app')
+    const $loading =document.querySelector('#loading')
     //loader
+    showCurrentWeather ($app,$loading)
     //date
     const $currentWeatherDate = document.querySelector('#current-weather-date')
     setCurrentDate($currentWeatherDate)
@@ -66,7 +74,7 @@ function configCurrentWeather(weather){
     //background
     const sunriseTime = new Date ( weather.sys.sunrise * 1000)
     const sunsetTime = new Date (  weather.sys.sunset *1000)
-    const $app =document.querySelector('#app')
+    
     const conditioncode = String (weather.weather[0].id).charAt(0)
     setBackground($app, conditioncode, solarStatus(sunriseTime, sunsetTime))
 
@@ -77,7 +85,7 @@ export default async function currentWeather() {
     console.log('esto pasa antes de getCurrentPosition')
     const {lat, lon, isError} = await getLatLon()
     if(isError) return console.log('Ah ocurrido un error ubicandote')
-    console.log(lat, lon)
+   // console.log(lat, lon)
 //      getCurrentPosition()
 //     .then((data) =>{
 //         console.log('hemos triunfado',data)
@@ -87,6 +95,9 @@ export default async function currentWeather() {
 
 //     })
 //     console.log('esto pasa despues de getCurrentPosition')
+   const {isError:currentWeatherError, data:weather} = await getCurrentWeather(lat, lon)
+   if (currentWeatherError) return console.log('oh! a ocurrido un error trayendo los datos del clima');
      configCurrentWeather(weather)
+
 //     console.log(weather);
  }
